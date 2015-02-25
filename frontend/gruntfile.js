03842dc91de7bed63aac1d88b9ce35b6ae4cbe51
dimentii <dimentii.dy@outlook.com>
2015-02-26 00:49:50 +0300

    Modifications to gruntfile.js. Folder structure improvements.

--- frontend/gruntfile.js
+++ frontend/gruntfile.js
@@ -5,24 +5,39 @@ module.exports = function(grunt) {
 		clean: {
 			options: { force: true },
 			all: {
-				src: ['app_build/**/*.*', 'dist/**/*.*']
-			}				
+				src: ['bin/**/*.*']
+			},
+			debug: {
+				src: ['bin/debug/**/*.*']
+			},
+			release: {
+				src: ['bin/release/**/*.*', 'bin/app_build/**/*.*']
+			}
 		},
 
 		less: {
-		    development: {
+		    debug: {
 		        options: {
-		            compress: true
+		            compress: false
 		        },
 		        files: {
-		            "app_build/css/bootstrap/bootstrap.min.css": "css/bootstrap/base.less"
+		            "bin/debug/css/bootstrap.css": "css/bootstrap/base.less"
 		        }
-		    }
+		    },
+			release: {
+				options: {
+					compress: true
+				},
+				files: {
+					"bin/app_build/css/bootstrap.min.css": "css/bootstrap/base.less"
+				}
+			}
 		},
 
 		ngAnnotate: {
 		    options: {
-		        singleQuotes: true
+		        singleQuotes: true,
+				flatten: true
 		    },
 		    all: {
 		        files: [
@@ -30,21 +45,21 @@ module.exports = function(grunt) {
                         expand: true,
                         cwd: 'app',
                         src: ['*.js'],
-                        dest: 'app_build/js',
+                        dest: 'bin/app_build/js',
                         ext: '.js'
                     },
 					{
 						expand: true,
 						cwd: 'app/views',
 						src: ['**/*.js'],
-						dest: 'app_build/js/views',
+						dest: 'bin/app_build/js/views',
 						ext: '.js'
 					},
 					{
 						expand: true,
-						cwd: 'app/main',
+						cwd: 'app/source',
 						src: ['**/*.js'],
-						dest: 'app_build/js/main',
+						dest: 'bin/app_build/js/source',
 						ext: '.js'
 					}
 		        ]
@@ -52,12 +67,100 @@ module.exports = function(grunt) {
 		},
 
 		copy: {
-			components: {
+			debug: {
 				files: [
 					{
 						expand: true,
-						src: ['path/*'],
-						dest: 'dest/',
+						cwd: 'app/components/',
+						src: [
+							'angular/angular.js',
+							'angular-animate/angular-animate.js',
+							'angular-route/angular-route.js',
+							'bootstrap/js/transition.js',
+							'bootstrap/js/collapse.js',
+							'jquery/dist/jquery.js'
+						],
+						dest: 'bin/debug/js/libs',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'app/',
+						src: [
+							'app.js',
+							'source/**/*.js',
+							'views/**/*.js',
+						],
+						dest: 'bin/debug/js',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						src: [
+							'css/*.css',
+							'app/components/normalize.css/normalize.css',
+						],
+						dest: 'bin/debug/css',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'fonts/',
+						src: [
+							'**/*.*'
+						],
+						dest: 'bin/debug/fonts',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'images/',
+						src: [
+							'**/*.*'
+						],
+						dest: 'bin/debug/images',
+						filter: 'isFile'
+					}
+				]
+			},
+			release: {
+				files: [
+					{
+						expand: true,
+						cwd: 'app/components/',
+						src: [
+							'angular/angular.min.js',
+							'angular-animate/angular-animate.min.js',
+							'angular-route/angular-route.min.js',
+							'bootstrap/js/transition.js',
+							'bootstrap/js/collapse.js',
+							'jquery/dist/jquery.min.js'
+						],
+						dest: 'bin/app_build/js/libs',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'fonts/',
+						src: [
+							'**/*.*'
+						],
+						dest: 'bin/release/fonts',
+						flatten: true,
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'images/',
+						src: [
+							'**/*.*'
+						],
+						dest: 'bin/release/images',
 						filter: 'isFile'
 					}
 				]
@@ -65,13 +168,13 @@ module.exports = function(grunt) {
 		},
 
 		uglify: {
-		    all: {
+		    release: {
 		        files: [
                     {
                         expand: true,
-                        cwd: 'app_build/js',
-                        src: ['**/*.js'],
-                        dest: 'app_build/js',
+                        cwd: 'bin/app_build/js',
+                        src: ['*.js', 'source/**/*.js', 'views/**/*.js', 'libs/transition.js', 'libs/collapse.js'],
+                        dest: 'bin/app_build/js',
                         ext: '.min.js'
                     }
 		        ]
@@ -79,38 +182,15 @@ module.exports = function(grunt) {
 		},
 
 		cssmin: {
-		    normalize: {
-		        src: 'app/bower_components/normalize.css/normalize.css',
-		        dest: 'app_build/css/normalize/normalize.min.css'
-		    },
-		    about: {
-		        src: 'css/about.css',
-                dest: 'app_build/css/about.min.css'
-		    },
-		    brand: {
-		        src: 'css/brand.css',
-		        dest: 'app_build/css/brand.min.css'
-		    },
-		    dress: {
-		        src: 'css/dress.css',
-		        dest: 'app_build/css/dress.min.css'
-		    },
-		    familylook: {
-		        src: 'css/familylook.css',
-		        dest: 'app_build/css/familylook.min.css'
-		    },
-		    main: {
-		        src: 'css/main.css',
-		        dest: 'app_build/css/main.min.css'
-		    },
-		    media: {
-		        src: 'css/media.css',
-		        dest: 'app_build/css/media.min.css'
-		    },
-		    sizeplus: {
-		        src: 'css/sizeplus.css',
-		        dest: 'app_build/css/sizeplus.min.css'
-		    }
+			release: {
+				files: [{
+					expand: true,
+					src: ['css/*.css', 'app/components/normalize.css/normalize.css'],
+					dest: 'bin/app_build/css',
+					flatten: true,
+					ext: '.min.css'
+				}]
+			}
 		},
 
 		concat: {
@@ -119,40 +199,62 @@ module.exports = function(grunt) {
 		    },
 		    libs:{
 		        src: [
-                    'app_build/js/bower_components/jquery/*.min.js',
-                    'app_build/js/bower_components/bootstrap/*.min.js',
-                    'app_build/js/bower_components/angular/angular.min.js',
-                    'app_build/js/bower_components/angular/angular-route.min.js',
-                    'app_build/js/app/custom/angular-touch-custom.min.js',
-                    'app_build/js/bower_components/angular/angular-animate.min.js'
+                    'bin/app_build/js/libs/jquery.min.js',
+                    'bin/app_build/js/libs/bootstrap.min.js',
+                    'bin/app_build/js/libs/angular.min.js',
+                    'bin/app_build/js/libs/angular-route.min.js',
+                    'bin/app_build/js/libs/angular-animate.min.js'
 		        ],
-                dest: 'dist/app_libs.js'
+                dest: 'bin/release/js/app_libs.min.js'
 		    },
-		    cust: {
+		    source: {
 		        src: [
-                    'app_build/js/app/app.min.js',
-                    'app_build/js/app/**/*Srvc.min.js',
-                    'app_build/js/app/**/*Directive.min.js',
-                    'app_build/js/app/**/*Ctrl.min.js'
+                    'bin/app_build/js/source/navigation/angular-touch-custom.min.js',
+					'bin/app_build/js/app.min.js',
+                    'bin/app_build/js/**/*Srvc.min.js',
+                    'bin/app_build/js/**/*Directive.min.js',
+                    'bin/app_build/js/**/*Ctrl.min.js'
 		        ],
-                dest: 'dist/app_build.js'
+                dest: 'bin/release/js/app_build.min.js'
 		    },
 		    styles: {
 		        options: {
 		            separator: ''
 		        },
 		        src: [
-                    'app_build/css/normalize/*.min.css',
-                    'app_build/css/bootstrap/*.min.css',
-                    'app_build/css/*.min.css'
+                    'bin/app_build/css/normalize.min.css',
+                    'bin/app_build/css/bootstrap.min.css',
+                    'bin/app_build/css/about.min.css',
+					'bin/app_build/css/brand.min.css',
+					'bin/app_build/css/dress.min.css',
+					'bin/app_build/css/familylook.min.css',
+					'bin/app_build/css/main.min.css',
+					'bin/app_build/css/media.min.css',
+					'bin/app_build/css/sizeplus.min.css'
 		        ],
-                dest: 'dist/styles.css'
+                dest: 'bin/release/css/styles.css'
 		    }
+		},
+
+		htmlmin: {
+			options: {
+				removeComments: true,
+				collapseWhitespace: true
+			},
+			files: {
+				'bin/release/html/navbar.html': 'app/source/navbar/navbar.html',
+				'bin/release/html/about.html': 'app/views/about/about.html',
+				'bin/release/html/brand.html': 'app/views/brand/brand.html',
+				'bin/release/html/dress.html': 'app/views/services/dress.html',
+				'bin/release/html/familylook.html': 'app/views/services/familylook.html',
+				'bin/release/html/sizeplus.html': 'app/views/services/sizeplus.html',
+				'bin/release/index.html': 'app/index/html'
+			}
 		}
 	});
-	
-	grunt.registerTask('default', ['build']);
-	grunt.registerTask('build', ['clean', 'less', 'ngAnnotate', 'copy', 'uglify', 'cssmin', 'concat']);
+
+	grunt.registerTask('debug', ['clean:debug', 'less:debug', 'copy:debug']);
+	grunt.registerTask('release', ['clean:release', 'less:release', 'ngAnnotate', 'copy:release', 'uglify:release', 'cssmin:release', 'concat'])
 	
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-ng-annotate');
@@ -160,5 +262,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-css');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 };
--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../app',
+    basePath: 'app',
 
 
     // frameworks to use
